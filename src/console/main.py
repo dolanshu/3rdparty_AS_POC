f48f7c0e041d7115512a57497395c5af0bd561e7
Dolan Shu <lingjia75@126.com>
2026-09-16 20:45:34 +0800

    chore: attribute the source licence headers to the author

--- src/console/main.py
+++ src/console/main.py
@@ -1,4 +1,4 @@
-# Copyright 2026 the 3rd-party AS POC authors.
+# Copyright 2026 Dolan Shu <dolan.d.shu@gmail.com>.
 # Licensed under the Apache License, Version 2.0 (the "License");
 # you may not use this file except in compliance with the License.
 # You may obtain a copy of the License at
--- tools/sippy_probe.py
+++ tools/sippy_probe.py
@@ -1,5 +1,5 @@
 #!/usr/bin/env python3
-# Copyright 2026 the 3rd-party AS POC authors.
+# Copyright 2026 Dolan Shu <dolan.d.shu@gmail.com>.
 #
 # Licensed under the Apache License, Version 2.0 (the "License");
 # you may not use this file except in compliance with the License.
